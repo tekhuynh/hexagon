bf9dca56ee7c4ab8125217b3cdb48eee688d3aad
Tek Huynh <5714652+tekhuynh@users.noreply.github.com>
2024-10-06 23:17:12 +1100

    Something works

--- sketch.js
+++ sketch.js
@@ -82,7 +82,7 @@ function make_2d_grid(cols, rows, fill = 0) {
     return grid;
 }
 
-function* calculate_offsets({ rows, cols, scale, height = 1, width = 1, v_offset = height, h_offset = width, v_shift = 0, h_shift = 0, parity = false, v_start = 0, h_start = 0}) {
+function* calculate_offsets({ rows, cols, scale, height = 1, width = 1, v_offset = height, h_offset = width, v_shift = 0, h_shift = 0, parity = false, v_start = 0, h_start = 0 }) {
 
     let minx = 1000
     let maxy = -1000
@@ -132,7 +132,7 @@ function* calculate_offsets({ rows, cols, scale, height = 1, width = 1, v_offset
                 j: j,
                 index: i + j * cols,
                 h_mid: h_start + scale * h_mid,
-                h_left: h_start+  scale * h_left,
+                h_left: h_start + scale * h_left,
                 h_right: h_start + scale * h_right,
                 v_top: v_start + scale * v_top,
                 v_bot: v_start + scale * v_bot,
@@ -146,18 +146,114 @@ function* calculate_offsets({ rows, cols, scale, height = 1, width = 1, v_offset
     }
 }
 
+class Parity {
+    constructor({h_flip = false, v_flip = false, h_shift = 0, v_shift = 0}) {
+        this.h_flip = h_flip;
+        this.v_flip = v_flip;
+        this.h_shift = h_shift;
+        this.v_shift = v_shift;
+    }
+}
+
+function* calculate_offsets2({ rows, cols, scale = 10, height = 1, width = 1, rise = 0, run = 0, v_offset = height, h_offset = width, v_start = 0, h_start = 0, parity = null }) {
+
+    let minx = 1000
+    let maxy = -1000
+    let miny = 1000
+    let maxx = -1000
+
+    let prev
+
+    for (let j = 0; j < rows; j++) {
+        for (let i = 0; i < cols; i++) {
+
+            let pair;
+
+            if (parity != null && i % 2 == 1) {
+
+                pair = { ...prev }
+                if (parity.h_flip) {
+                    pair.h_left = prev.h_right
+                    pair.h_right = prev.h_left
+                }
+                if (parity.v_flip) {
+                    pair.v_top = prev.v_bot
+                    pair.v_bot = prev.v_top
+                }
+                if (parity.h_shift) {
+                    pair.h_left += parity.h_shift * scale
+                    pair.h_right += parity.h_shift * scale
+                    pair.h_mid += parity.h_shift* scale
+                }
+                if (parity.v_shift) {
+                    pair.v_top += parity.v_shift* scale
+                    pair.v_bot += parity.v_shift* scale
+                    pair.v_mid += parity.v_shift* scale
+                }
+            } else {
+
+                // offset is how spread out the shapes are
+                // start from mid height of the shape and then go up or down, left or right
+                // these are relative to the center
+
+                let h_mid = h_start + width / 2 + i * h_offset + run * j
+                let h_left = h_mid - width / 2;
+                let h_right = h_mid + width / 2;
+
+                let v_mid = v_start + height / 2 + j * v_offset + rise * i
+                let v_top = v_mid - height / 2;
+                let v_bot = v_mid + height / 2;
+
+                pair = {
+                    h_mid: scale * h_mid,
+                    h_left: scale * h_left,
+                    h_right: scale * h_right,
+                    v_top: scale * v_top,
+                    v_bot: scale * v_bot,
+                    v_mid:scale *  v_mid,
+                }
+                prev = pair
+            }
+
+            minx = Math.min(minx, pair.h_left)
+            maxx = Math.max(maxx, pair.h_right)
+            miny = Math.min(miny, pair.v_top)
+            maxy = Math.max(maxy, pair.v_bot)
+
+            pair = {
+                ...pair,
+                i: i,
+                j: j,
+                index: i + j * cols,
+                minx: h_start + scale * minx,
+                maxx: h_start + scale * maxx,
+                miny: v_start + scale * miny,
+                maxy: v_start + scale * maxy
+            };
+
+            yield pair
+        }
+
+    }
+}
+
+
 function setup() {
 
     let three = Math.sqrt(3) / 2;
     offsets = make_2d_grid(cols, rows, null)
-    let calculations = calculate_offsets({
+    let calculations = calculate_offsets2({
         rows: rows,
         cols: cols,
         scale: scale,
+        run: 1/2,
         height: three,
         width: 1,
         h_offset: 1/2,
-        parity: true,
+        parity: new Parity({
+            v_flip: true,
+            h_shift: 1/2
+        }),
     });
 
     for (let o of calculations) {
@@ -172,7 +268,7 @@ function setup() {
     queue.enqueue(offsets[midx][midy]);
     set_grid_random();
     let last = offsets[cols - 1][rows - 1]
-    createCanvas(min(1000, last.maxx - last.minx), min(1000,last.maxy - last.miny));
+    createCanvas(min(1000, last.maxx - last.minx), min(1000, last.maxy - last.miny));
     noLoop();
 }
 
@@ -205,13 +301,13 @@ function draw() {
         for (let j = 0; j < rows; j++) {
             let o = offsets[i][j];
             if (grid[i][j] == 1) {
-                fill(100 * (!(j % 2)) + 100);
+                fill(100 * ((i % 2)) + 100);
                 triangle(o.h_left, o.v_bot, o.h_right, o.v_bot, o.h_mid, o.v_top);
                 //triangle(o.h_left, o.v_bot, o.h_left, o.v_top, o.h_right, o.v_mid);
-                fill(0)
+                fill(255)
                 textSize(7);
                 textAlign(CENTER, CENTER);
-                text(o.index + ":" +i + "," + j, o.h_mid, o.v_mid);
+                text(o.index + ":" + i + "," + j, o.h_mid, o.v_mid);
             }
         }
     }
